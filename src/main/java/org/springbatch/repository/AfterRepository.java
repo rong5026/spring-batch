e7508518bed7a14e1574889f57fedb4233542115
rong5026 <rong5026@naver.com>
2025-03-14 17:20:35 +0900

    table 분리

---
+++ src/main/java/org/springbatch/repository/AfterRepository.java
@@ -0,0 +1,8 @@
+package org.springbatch.repository;
+
+import org.springbatch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+
+}
---
+++ src/main/java/org/springbatch/repository/BeforeRepository.java
@@ -0,0 +1,7 @@
+package org.springbatch.repository;
+
+import org.springbatch.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+}
