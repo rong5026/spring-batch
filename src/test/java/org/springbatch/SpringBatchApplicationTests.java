9b388c132857f2d95a4942b1f73be934719c24f6
rong5026 <rong5026@naver.com>
2025-03-14 16:39:37 +0900

    init

---
+++ src/test/java/org/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
